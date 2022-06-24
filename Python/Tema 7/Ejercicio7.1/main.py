d99920b911d727b1ca10c41de71f5787f3ac0c9d
Panurai <77552859+Panurai@users.noreply.github.com>
2022-06-24 20:25:12 -0300

    Add files via upload

---
+++ Python/Tema 7/Ejercicio7.1/main.py
@@ -0,0 +1,12 @@
+import operaciones.operaciones as op
+
+def main():
+    a= 5
+    b= 2
+    print(op.suma(a,b))
+    print(op.resta(a, b))
+    print(op.multiplica(a, b))
+    print(op.divide(a, b))
+
+if __name__ == '__main__':
+    main()
---
+++ Python/Tema 7/Ejercicio7.1/operaciones/operaciones.py
@@ -0,0 +1,23 @@
+def suma(a,b):
+    """"
+    Suma a + b
+    """
+    return a+b
+
+def resta(a,b):
+    """"
+    Resta a-b
+    """
+    return a-b
+
+def multiplica(a,b):
+    """
+    Mulitplica a por b
+    """
+    return a*b
+
+def divide(a,b):
+    """"
+    Divide a por b
+    """
+    return a/b
